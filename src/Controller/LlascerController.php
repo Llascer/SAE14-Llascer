5487ca8001629597ffe9862cf322d454af7ce949
Llascer <llascerchemar@gmail.com>
2025-01-19 22:32:00 +0100

    Loisirs et pages d'acceuil

--- src/Controller/LlascerController.php
+++ src/Controller/LlascerController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class LlascerController extends AbstractController{
+final class LlascerController extends AbstractController
+{
+    // Page d'accueil
     #[Route('/llascer', name: 'app_llascer')]
     public function index(): Response
     {
@@ -14,4 +16,50 @@ final class LlascerController extends AbstractController{
             'controller_name' => 'LlascerController',
         ]);
     }
+
+    // Page "Home" (page principale)
+    #[Route('/llascer/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('llascer/home.html.twig', [ 
+            'title' => "Bienvenue", 
+            'age' => 31 
+        ]);
+    }
+
+    // Page "Loisirs"
+    #[Route('/llascer/loisirs', name: 'app_loisirs')]
+    public function loisirs(): Response
+    {
+        return $this->render('llascer/loisir.html.twig', [
+            'title' => "Mes Loisirs"
+        ]);
+    }
+
+    // Page "CV"
+    #[Route('/llascer/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('llascer/cv.html.twig', [
+            'title' => "Mon CV"
+        ]);
+    }
+
+    // Page "E-Portfolio"
+    #[Route('/llascer/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('llascer/portfolio.html.twig', [
+            'title' => "Mon Portfolio"
+        ]);
+    }
+    //Page "Experiences"
+    #[Route('/llascer/experiences', name: 'app_experiences')]
+public function experiences(): Response
+{
+    return $this->render('llascer/experiences.html.twig', [
+        'title' => 'Mes Expériences'
+    ]);
+}
+
 }
